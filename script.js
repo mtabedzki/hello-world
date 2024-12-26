1b05fea9e7568115bf916b60aa7b934ab6784cdb
Marek Tabędzki <marek.tabedzki@gmail.com>
2024-12-26 12:33:45 +0100

    skrypt

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'Hello, world!';
+    document.body.appendChild(paragraph);
+});
